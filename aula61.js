d6f2d0d75d09d216dbdde92e0366d1512a4f66ee
marcelodalves <43051943+marcelodalves@users.noreply.github.com>
2019-05-03 21:07:26 -0300

    Create aula61.js

---
+++ aula61.js
@@ -0,0 +1,11 @@
+function acendeLampada(){
+	document.getElementById("luz").src="imagens/acesa.jpg";
+}
+
+function apagaLampada(){
+	document.getElementById("luz").src="imagens/apagada.jpg";
+}
+
+function quebradaLampada(){
+	document.getElementById("luz").src="imagens/quebra.jpg";
+}
