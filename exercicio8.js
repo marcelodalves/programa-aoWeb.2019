ab1fbd87578831b8bc7b75a3dadbfd4dd2114260
marcelodalves <43051943+marcelodalves@users.noreply.github.com>
2019-05-24 21:32:13 -0300

    Create exercicio8.js

---
+++ exercicio8.js
@@ -0,0 +1,11 @@
+function escrever(aff){
+	var soma = parseInt(valorA) + parseInt(valorC);
+	info="Soma de A+C: "+soma+"\n";
+	aff.value+=info;
+}
+
+function escrever2(aff){
+	var mult = (valorB)*(valorD);
+	info="Multiplicação de B*D: "+mult+"\n";
+	aff.value+=info;	
+}
